c8b3176930b8d06ea72ecf29d4889e2120271d80
Prasad Bebale <119681991+Prasad103@users.noreply.github.com>
2023-10-04 20:28:05 +0530

    Create Hollow_rhombus.java

---
+++ Hollow_rhombus.java
@@ -0,0 +1,23 @@
+public class Hollow_rhombus {
+    public static void hollow_rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+    
+            //spaces - (n-1)
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+    
+            for(int j=1; j<=n; j++) {
+                if(i == 1 ||  i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        hollow_rhombus(5);
+    }
+}
